1b6f756393126953655c934c6b73e343d5a04b6e
jbwexler <jbwexler@tutanota.com>
2022-07-24 19:13:48 -0500

    change gitmodule urls to origin

--- sourcedata/freesurfer/sub-xp222/scripts/recon-all-rh.cmd
+++ sourcedata/freesurfer/sub-xp222/scripts/recon-all-rh.cmd
@@ -1,1 +1,1 @@
-/annex/objects/MD5E-s16097--32a76bb73125435027cb44c3661de66d.cmd
+../../../../.git/annex/objects/WQ/gV/MD5E-s16097--32a76bb73125435027cb44c3661de66d.cmd/MD5E-s16097--32a76bb73125435027cb44c3661de66d.cmd
--- sourcedata/freesurfer/sub-xp222/scripts/recon-all.cmd
+++ sourcedata/freesurfer/sub-xp222/scripts/recon-all.cmd
@@ -1,1 +1,1 @@
-/annex/objects/MD5E-s8725--e7a49bc9d8e210d7b957e36c6c1ff1c3.cmd
+../../../../.git/annex/objects/0X/4M/MD5E-s8725--e7a49bc9d8e210d7b957e36c6c1ff1c3.cmd/MD5E-s8725--e7a49bc9d8e210d7b957e36c6c1ff1c3.cmd
